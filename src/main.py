a64997349824fa37fab905aac6231bf963df60fc
feersantana5 <feersantana5@gmail.com>
2026-03-01 19:30:37 +0100

    wip

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import sys
+from pathlib import Path
+
 sys.path.append('.')
 
 import uvicorn
@@ -6,4 +8,4 @@ import warnings
 warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=False, env_file='.env')
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=False, env_file=str(Path(__file__).parent.parent / '.env'))
